a2978217016c043f6f5c0efb3c7d74b4b739117d
weaam-12 <abukafweaam2001@gmail.com>
2025-07-28 20:17:03 +0300

    fix

--- src/pages/Childern.jsx
+++ src/pages/Childern.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../AuthContext.jsx';
 import './Children.css';
 
 const Children = () => {
