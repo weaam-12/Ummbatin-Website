b1defbbbb2e22d75be0f5625ffb64c63d9c42414
weaam-12 <abukafweaam2001@gmail.com>
2025-07-28 15:14:10 +0300

    FamillyLoahgتا

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,9 +58,9 @@ const Navbar = () => {
     // روابط المستخدم العادي
     const userLinks = [
         { path: "/", icon: <FaHome />, text: t("common.home") },
-        { path: "/complaints", icon: <FaExclamationCircle />, text: t("servic.complaints") },
-        { path: "/payments", icon: <FaMoneyBillWave />, text: t("transactions.payments") },
-        { path: "/emergency", icon: <FaBell />, text: t("servic.esemergency") },
+        { path: "/complaints", icon: <FaExclamationCircle />, text: t("services.complaints") },
+        { path: "/payments", icon: <FaMoneyBillWave />, text: t("services.transactions") },
+        { path: "/emergency", icon: <FaBell />, text: t("services.esemergency") },
         { path: "/garbage-complaints", icon: <FaTrash />, text: t("services.garbage") },
         { path: "/children", icon: <FaGraduationCap />, text: t("navbar.education") }
     ];
